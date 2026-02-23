76a3fba55c767de18291c65c8e517301260b8863
GMarcell <grand1310marcell@gmail.com>
2026-02-23 22:02:27 +0700

    feat: set global prefix for API routes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
