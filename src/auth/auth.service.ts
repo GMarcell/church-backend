e519353f9bdc74f30d20f93eac348582367cc241
GMarcell <grand1310marcell@gmail.com>
2026-02-24 16:40:37 +0700

    delete console.log

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -37,17 +37,11 @@ export class AuthService {
       where: { email },
     });
 
-    console.log(user);
-
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    console.log(password);
-    console.log(isPasswordValid);
-    console.log(user?.password);
 
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid credentials');
