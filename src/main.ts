2d2de2176ea8dccf134ea581e3c319c42fe788e7
GMarcell <grand1310marcell@gmail.com>
2026-04-02 10:39:50 +0700

    added health check

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { RequestMethod } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
@@ -5,13 +6,16 @@ import { ResponseInterceptor } from './common/interceptor/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+  const port = Number(process.env.PORT ?? 3000);
 
   app.enableCors({
     origin: ['http://localhost:3001', 'https://your-frontend.com'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
